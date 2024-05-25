03039b7bc1b88c104f3750c4ad490d31d88b2c2e
wangsong <wangsong0210@gmail.com>
2024-05-25 10:44:56 +0800

    m

--- src/main/java/com/tencent/wxcloudrun/controller/IndexController.java
+++ src/main/java/com/tencent/wxcloudrun/controller/IndexController.java
@@ -1,7 +1,8 @@
 package com.tencent.wxcloudrun.controller;
 
 import com.tencent.wxcloudrun.model.Msg;
-import org.springframework.stereotype.Controller;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 public class IndexController {
 
+    public final static Logger logger = LoggerFactory.getLogger(IndexController.class);
     /**
      * 主页页面
      *
@@ -26,6 +28,7 @@ public class IndexController {
 
     @PostMapping("/search")
     public String search(@RequestBody Msg msg) {
+        logger.info("msg: {}", msg);
         return msg.getContent();
     }
 
